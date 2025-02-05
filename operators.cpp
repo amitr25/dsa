d75b5ce2bfc36f421116c51ed5a0038e9345c540
amitr25 <14054amitraj@gmail.com>
2025-02-06 00:34:45 +0530

    operators

---
+++ operators.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ////airthmetic operator
+    // int a=5 , b=3;
+
+    // cout<<"sum ="<<(a+b)<<endl;
+    // cout<<"difference ="<<(a-b)<<endl;
+    // cout<<"product ="<<(a*b)<<endl;
+    // cout<<"division ="<<(a/b)<<endl;
+    // cout<<"modulo ="<<(a%b)<<endl;
+    // return 0;
+
+    ////relational operator
+    // cout<<(5<3)<<endl;
+    // cout<<(5>3)<<endl;
+    // cout<<(5<=3)<<endl;
+    // cout<<(5>=3)<<endl;
+    // cout<<(5==3)<<endl;
+    // cout<<(5!=3)<<endl;
+
+    ////logical operator
+    // cout<<((4<5) && (5<4 ))<<endl;
+    // cout<<((4<5)  || (5<4 ))<<endl;
+    // cout<<((5<5) || (5<4 ))<<endl;
+    // cout<<!(4<5)<<endl;
+
+    ////sum of two numbers
+
+    int a,b;
+
+    cout<<"enter your first number="<<endl;
+    cin>>a;
+    cout<<"enter your second number="<<endl;
+    cin>>b;
+    cout<<"sum of two numbers="<<(a+b)<<endl;
+    return 0;
+
+
+}
