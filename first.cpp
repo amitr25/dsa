6f90ada9b48f3c3f67d9a26edf01b999a6b8300f
amitr25 <14054amitraj@gmail.com>
2025-02-05 23:05:42 +0530

    first

---
+++ first.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+//this is starting point
+//datatypes
+
+// int main(){
+//     int age=21;
+//     //implicit typecasting
+//     char grade='a';
+//     int val=grade;
+//     float PI=3.14f;
+//     cout<<val<<endl;
+    
+
+
+//     bool isAdult =false;
+//     //explicit typecasting
+//     double price=100.55;
+//     int newPrice=(int)price;
+//     cout<<newPrice<<endl;
+//     cout<<"age is "<<age<<endl <<isAdult<<endl;
+//     cout <<sizeof(age)<<endl;
+//     cout<<sizeof(price);
+//     return 0;
+
+// }
+// // boilerplate code
+
+// input 
+int main(){
+    int age;
+    cout<<"enter your age:";
+    cin>>age;
+    cout<<"your age is :"<<age<<endl;
+}
